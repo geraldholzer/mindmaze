7aa49b5fc00b4f0826d4a3ddbfca9d292e0a1423
SebastianHerth <120731156+SebastianHerth@users.noreply.github.com>
2024-02-22 19:55:33 +0100

    Design übernommen und style.css erweitert

--- home.php
+++ home.php
@@ -103,7 +103,44 @@
         </div>
 
     </div>
-
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">IU-Mindmaze</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php">Home</a>
+          </li>
+          <!-- href auf singleplayer.html hinzugefügt Gerald -->
+          <li class="nav-item">
+            <a class="nav-link" href="./src/html-php-view/singleplayer.html">Solo</a>
+          </li>
+            <!-- href auf lobby.html hinzugefügt Gerald -->
+          <li class="nav-item">
+            <a class="nav-link" href="./src/html-php-view/lobby.html">Multiplayer</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Konto
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Statistik</a></li>
+              <li><a class="dropdown-item" href="#">Fragen</a></li>
+              <li><a class="dropdown-item" href="#">Passwort ändern</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Abmelden</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
 </body>
 
