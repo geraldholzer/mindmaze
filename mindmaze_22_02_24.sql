f42d60e6eef155679dee2114451afc21c966aa59
Gerald Holzer <gerald_holzer@gmx.at>
2024-02-22 21:28:54 +0100

    Fragen zur Datenbank hinzugefügt

--- mindmaze_22_02_24.sql
+++ mindmaze_22_02_24.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 18. Feb 2024 um 17:55
+-- Erstellungszeit: 22. Feb 2024 um 21:27
 -- Server-Version: 10.4.32-MariaDB
 -- PHP-Version: 8.2.12
 
@@ -33,6 +33,72 @@ CREATE TABLE `antworten` (
   `Text` varchar(255) DEFAULT NULL,
   `Korrekt` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `antworten`
+--
+
+INSERT INTO `antworten` (`AntwortID`, `FragenID`, `Text`, `Korrekt`) VALUES
+(1, 0, '5', 1),
+(2, 0, '3', 0),
+(3, 0, '2', 0),
+(4, 0, '8', 0),
+(5, 1, '13', 0),
+(6, 1, '5', 1),
+(7, 1, '3', 0),
+(8, 1, '12', 0),
+(9, 5, '\r\nunverzüglich', 1),
+(10, 5, 'nach einer Woche', 0),
+(11, 5, 'niemals', 0),
+(12, 5, 'nach einem Monat', 0),
+(13, 2, '\r\nNur dann, wenn schuldhaft gehandelt wird und keine Rechtfertigung vorliegt.', 1),
+(14, 2, 'nein', 0),
+(15, 2, 'ja', 0),
+(16, 2, '\r\nNur dann, wenn fahrlässig gehandelt wird.', 0),
+(17, 6, '§ 631 BGB', 0),
+(18, 6, '§ 611 BGB', 0),
+(19, 6, '§ 433 BGB', 1),
+(20, 6, '§ 13 BGB', 0),
+(21, 7, 'Nacherfüllung', 1),
+(22, 7, 'Minderung', 0),
+(23, 7, 'Schadensersatz', 0),
+(24, 7, 'Rücktritt', 0),
+(25, 8, 'Schenkung', 0),
+(26, 8, 'Mietvertrag', 0),
+(27, 8, 'Kaufvertrag', 0),
+(28, 8, 'Werkvertrag', 1),
+(29, 9, 'bei Kaufverträgen', 0),
+(30, 9, 'bei allen Verträgen über Waren und/oder Dienstleistungen', 1),
+(31, 9, 'nie', 0),
+(32, 9, 'bei Mietverträgen', 0),
+(33, 10, 'bei wettbewerbsbeschränkenden Verhaltensweisen von Unternehmen', 1),
+(34, 10, 'bei Gewährleistungsansprüchen von Kunden', 0),
+(35, 10, 'bei unzulässigen AGB-Klauseln', 1),
+(36, 10, 'bei unzulässigen Werbemaßnahmen', 0),
+(37, 11, 'Vorgabe de Hardware für Nutzung von Software', 0),
+(38, 11, 'Einräumung der Nutzungsrechte', 1),
+(39, 11, 'Dauer der Nutzung der Software', 0),
+(40, 11, 'finanzielle Fragen der Nutzung', 0),
+(41, 12, 'individueller Vertrag', 0),
+(42, 12, 'vertragliche Regelungen eines Verwenders für eine Vielzahl von Verträgen mit unterschiedlichen Vertragspartnern', 1),
+(43, 12, 'unverbindliche rechtliche Informationen eines Anbieters von Waren oder Dienstleistungen', 0),
+(44, 12, 'Vertragsbedingungen für Bestellung im E-Commerce', 0),
+(45, 13, 'Die AGB sind nicht wirksam in den geschlossenen Vertrag einbezogen worden.', 1),
+(46, 13, 'Es gilt das Bürgerliche Gesetzbuch (BGB).', 0),
+(47, 13, 'keine', 0),
+(48, 13, 'Die AGB müssen lesbar gemacht werden.', 0),
+(49, 14, 'Unterlassung', 1),
+(50, 14, 'Minderung', 0),
+(51, 14, 'Nachbesserung', 0),
+(52, 14, 'Garantie', 0),
+(53, 15, 'Open Source Software darf unter Beachtung der Lizenzvorgaben beliebig oft kopiert, verbreitet und genutzt werden.', 1),
+(54, 15, 'der Zweck der Software', 0),
+(55, 15, 'keiner', 0),
+(56, 15, 'der Ort der Programmierung', 0),
+(57, 3, 'Fotografie', 0),
+(58, 3, 'Buch', 0),
+(59, 3, 'Logo', 0),
+(60, 3, 'technische Erfindung', 1);
 
 -- --------------------------------------------------------
 
@@ -74,6 +140,27 @@ CREATE TABLE `fragen` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Daten für Tabelle `fragen`
+--
+
+INSERT INTO `fragen` (`FragenID`, `KursID`, `FragentypID`, `FrageText`, `InfoText`, `Status`, `Änderungsdatum`) VALUES
+(0, 2, 1, 'Wie viele Bücher hat das Bürgerliche Gesetzbuch (BGB)?', 'Nachzulesen im BGB', 1, '2024-02-20'),
+(1, 2, 1, 'Wie viele „Kaufleute“-Begriffe kennt das Handelsgesetzbuch (HGB)?', 'Im HGB nachzulesen', 1, '2024-02-20'),
+(2, 2, 1, 'Ist das Hacking von IT-Systemen immer strafbar?', 'IM STGB nachzulesen', 1, '2024-02-20'),
+(3, 2, 1, 'Was kann nach dem Patentrecht geschützt sein?', 'Details im Patentgesetz', 1, '2024-02-22'),
+(5, 2, 1, 'Wann muss ein Kaufmann einen bei Lieferung sofort erkannten Mangel an einer gelieferten Ware nach dem Gesetz rügen?', 'Nachzulesen im HGB', 1, '2024-02-20'),
+(6, 2, 1, 'Welche der nachfolgend genannten gesetzlichen Vorschriften regelt den Kaufvertrag?', 'Details im BGB', 1, '2024-02-22'),
+(7, 2, 1, 'Welches Recht muss der Käufer bei einem Mangel einer Kaufsache zuerst geltend machen?', 'Details im BGB', 1, '2024-02-22'),
+(8, 2, 1, 'Welche Vertragsart findet auf den Erstellungsvertrag von Software Anwendung?', 'Details im BGB', 1, '2024-02-22'),
+(9, 2, 1, 'Wann muss eine öffentliche Einrichtung das Vergaberecht beachten?', 'Details im BGB', 1, '2024-02-22'),
+(10, 2, 1, 'Wann greift das Kartellrecht ein?', 'Details im BGB', 1, '2024-02-22'),
+(11, 2, 1, 'Was regelt die Lizenz bei der Softwareüberlassung?', 'Details im BGB', 1, '2024-02-22'),
+(12, 2, 1, 'Was sind Allgemeine Geschäftsbedingungen (AGB)?', 'Details im BGB', 1, '2024-02-22'),
+(13, 2, 1, 'Was kann die Folge sein, wenn Allgemeine Geschäftsbedingungen (AGB) nicht lesbar sind?', 'Details im BGB', 1, '2024-02-22'),
+(14, 2, 1, 'Welcher Anspruch kann zur Durchsetzung von Ansprüchen aus dem Urheberrecht geltend macht werden?', 'Details im BGB', 1, '2024-02-22'),
+(15, 2, 1, 'Was ist der Unterschied zwischen Open Source Software und „normaler“ Software?', 'Details im BGB', 1, '2024-02-22');
+
+--
 -- Trigger `fragen`
 --
 DELIMITER $$
@@ -95,6 +182,13 @@ CREATE TABLE `fragentyp` (
   `FragentypID` int(12) NOT NULL,
   `Beschreibung` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Daten für Tabelle `fragentyp`
+--
+
+INSERT INTO `fragentyp` (`FragentypID`, `Beschreibung`) VALUES
+(1, 'multiplechoice');
 
 -- --------------------------------------------------------
 
@@ -131,16 +225,6 @@ CREATE TABLE `spiele` (
   `KursID` int(11) DEFAULT NULL,
   `Spielname` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Daten für Tabelle `spiele`
---
-
-INSERT INTO `spiele` (`SpieleID`, `SpielmodiID`, `BenutzerIDSpieler1`, `BenutzerIDSpieler2`, `SitzungAktiv`, `BenutzerFragen`, `KursID`, `Spielname`) VALUES
-(6, 1, NULL, NULL, NULL, NULL, 2, 'game'),
-(7, 1, NULL, NULL, NULL, NULL, 2, 'meinspiel'),
-(12, 1, NULL, NULL, NULL, NULL, 2, 'fddd'),
-(13, 1, NULL, NULL, NULL, NULL, 2, 'neues Spiel');
 
 -- --------------------------------------------------------
 
@@ -344,7 +428,7 @@ ALTER TABLE `zugriffsrechte`
 -- AUTO_INCREMENT für Tabelle `antworten`
 --
 ALTER TABLE `antworten`
-  MODIFY `AntwortID` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `AntwortID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- AUTO_INCREMENT für Tabelle `benutzer`
@@ -356,13 +440,13 @@ ALTER TABLE `benutzer`
 -- AUTO_INCREMENT für Tabelle `fragen`
 --
 ALTER TABLE `fragen`
-  MODIFY `FragenID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `FragenID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT für Tabelle `fragentyp`
 --
 ALTER TABLE `fragentyp`
-  MODIFY `FragentypID` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `FragentypID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT für Tabelle `kurse`
@@ -374,7 +458,7 @@ ALTER TABLE `kurse`
 -- AUTO_INCREMENT für Tabelle `spiele`
 --
 ALTER TABLE `spiele`
-  MODIFY `SpieleID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `SpieleID` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT für Tabelle `spielmodi`
