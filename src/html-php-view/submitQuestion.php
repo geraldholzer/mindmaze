04e985d45f658af144617a5fec18e30a777efb8e
SebastianHerth <120731156+SebastianHerth@users.noreply.github.com>
2024-03-13 20:23:07 +0100

    SubmitQuestion erweitert

--- src/html-php-view/submitQuestion.php
+++ src/html-php-view/submitQuestion.php
@@ -1,26 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
-        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-        crossorigin="anonymous"></script>
-
+    <title>Neue Frage einreichen</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" href="../css/main.css">
-
 </head>
 
 <body>
@@ -33,34 +21,84 @@
             <h1>Neue Frage einreichen</h1>
         </div>
         <div class="row mt-4">
-            <Form>
-                <div class="container">
-                    <div class="form-group col-5 mt-4">
-                        <label for="selectModul">Modul</label>
-                        <select class="form-control" id="selectModul">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>5</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-5 mt-4">
-                        <label for="selectModul">Modul</label>
-                        <input type="text" class="form-control" id="textQuestion" placeholder="Frage">
-                    </div>
-                    <div class="form-group col-5 mt-4">
-                        <label for="textQuestion">Modul</label>
-                        <input type="text" class="form-control" id="textQuestion" placeholder="Frage">
-                    </div>
-                    <div class="form-group col-5 mt-4">
-                        <label for="textAnswer">Modul</label>
-                        <input type="text" class="form-control" id="textAnswer" placeholder="Antwort">
-                    </div>
+            <div class="container">
+                <div class="form-group col-5 mt-4">
+                    <label for="selectModul">Modul</label>
+                    <select class="form-control" id="selectModul">
+                        <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname = "mindmaze";
+                        $con = new mysqli($servername, $username, $password, $dbname);
+                        if ($con->connect_error) {
+                            die("Es konnte keine Verbindung zur Datenbank hergestellt werden" . $con->connect_error);
+                        }
+                        $sql = "SELECT * FROM kurse";
+                        $result = $con->query($sql);
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<option value='" . $row['KursID'] . "'>" . $row["Beschreibung"] . "</option>";
+                        }
+                        ?>
+                    </select>
                 </div>
-            </Form>
+                <div class="form-group col-5 mt-4">
+                    <label for="textQuestion">Frage</label>
+                    <input type="text" class="form-control" id="textQuestion" placeholder="Frage">
+                </div>
+                <div class="form-group col-5 mt-4">
+                    <label for="textInfotext">Infotext (Optional)</label>
+                    <input type="text" class="form-control" id="textInfotext" placeholder="Infotext">
+                </div>
+                <div class="form-group col-5 mt-4">
+                    <label for="textAnswer">Antwort</label>
+                    <input type="text" class="form-control" id="textAnswer" placeholder="Antwort">
+                </div>
+                <div class="form-group col-5 mt-4">
+                    <button class="button-short mt-4" id="submitBtn">Abschicken</button>
+                </div>
+            </div>
         </div>
     </div>
+    <script>
+        document.getElementById('submitBtn').addEventListener('click', function () {
+            // Daten sammeln
+            var modul = document.getElementById('selectModul').value;
+            var frage = document.getElementById('textQuestion').value;
+            var infotext = document.getElementById('textInfotext').value;
+
+            var antwort = document.getElementById('textAnswer').value;
+
+            // Daten vorbereiten
+            var data = new FormData();
+            data.append('modul', modul);
+            data.append('frage', frage);
+            data.append('antwort', antwort);
+            data.append('infotext', infotext);
+
+            // Anfrage senden
+            fetch('../server/uploadQuestion.php', {
+                method: 'POST',
+                body: data
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Fehler beim Senden der Daten.');
+                    }
+                    return response.text();
+                })
+                .then(data => {
+                    // Erfolgsmeldung anzeigen oder weitere Aktionen ausführen
+                    console.log('Daten erfolgreich eingereicht:', data);
+                    // Hier kannst du weitere Aktionen ausführen, z.B. eine Erfolgsmeldung anzeigen
+                })
+                .catch(error => {
+                    console.error('Fehler:', error);
+                    // Hier kannst du Fehlerbehandlung durchführen, z.B. eine Fehlermeldung anzeigen
+                });
+                document.getElementById("textQuestion").value = "";
+        });
+    </script>
 </body>
 
 </html>
