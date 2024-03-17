7c46e2b2695c00ba2f8a2029894581e21006b0ff
SebastianHerth <120731156+SebastianHerth@users.noreply.github.com>
2024-03-17 21:14:48 +0100

    UserManagement um Paginierung erweitert

--- src/html-php-view/userManagement.php
+++ src/html-php-view/userManagement.php
@@ -34,16 +34,28 @@
         echo "<h3>Du scheinst nicht über die notwendigen Zugriffsrechte zu verfügen!</h3>";
         die();
     }
-    include ("navbar.php");
-    // Verbinde mit der Datenbank
-    // $servername = "localhost";
-    // $username = "root";
-    // $password = "";
-    // $dbname = "mindmaze";
+    include("navbar.php");
+
     include "../html-php-view/dbconnect.php";
     $con = new mysqli($servername, $username, $password, $dbname);
     if ($con->connect_error) {
         die("Es konnte keine Verbindung zur Datenbank hergestellt werden" . $con->connect_error);
+    }
+
+    $sql = "SELECT COUNT(*) AS total FROM benutzer";
+    $result = mysqli_query($con, $sql);
+
+    // Überprüfe, ob die Abfrage erfolgreich war
+    if ($result) {
+        // Erhalte die Zeile mit den Ergebnissen
+        $row = mysqli_fetch_assoc($result);
+
+        // Gesamtanzahl der Datensätze
+        $totalRecords = $row['total'];
+
+    } else {
+        // Fehlerbehandlung, wenn die Abfrage fehlschlägt
+        echo "Fehler: " . mysqli_error($con);
     }
 
     // Wenn das Formular gesendet wurde
@@ -60,23 +72,29 @@
     $qryVorname = isset($_GET['Vorname']) ? $_GET['Vorname'] : "";
     $qryNachname = isset($_GET['Nachname']) ? $_GET['Nachname'] : "";
     $qryEmail = isset($_GET['Email']) ? $_GET['Email'] : "";
+    $maxRecords = isset($_GET['MaxRecords']) ? $_GET['maxRecords'] : 3;
+    $page = isset($_GET['Page']) ? $_GET['Page'] : 1;
+    $offset = $maxRecords * ($page - 1);
+    $lastPage = ceil($totalRecords / $maxRecords);
+    $nextPage = $page + 1;
+    $previousPage = $page - 1;
 
     // Baue die WHERE-Bedingung basierend auf den eingereichten Formularwerten auf
     $whereClause = "WHERE 1"; // Initialisiere die WHERE-Klausel mit 1, um alle Datensätze zu erhalten
     
     // Füge die Bedingungen für Vorname, Nachname und E-Mail hinzu, wenn sie nicht leer sind
     if (!empty($qryVorname)) {
-        $whereClause .= " AND Vorname like '" . "%"    . $qryVorname . "%" . "'";
+        $whereClause .= " AND Vorname like '" . "%" . $qryVorname . "%" . "'";
     }
     if (!empty($qryNachname)) {
-        $whereClause .= " AND Nachname like '" . "%"    . $qryNachname . "%" . "'";
+        $whereClause .= " AND Nachname like '" . "%" . $qryNachname . "%" . "'";
     }
     if (!empty($qryEmail)) {
-        $whereClause .= " AND Email like '" . "%" . $qryEmail . "%" ."'";
+        $whereClause .= " AND Email like '" . "%" . $qryEmail . "%" . "'";
     }
 
     // Führe die SQL-Abfrage mit der WHERE-Klausel aus
-    $sql = "SELECT * FROM benutzer $whereClause";
+    $sql = "SELECT * FROM benutzer $whereClause LIMIT $maxRecords OFFSET $offset";
     $result = $con->query($sql);
     ?>
 
@@ -86,6 +104,8 @@
             <div class="col-10">
                 <h1 class="mt-3">Benutzer</h1>
                 <form class="mt-3" method="post">
+                <button class="button-short" type="submit">Änderungen bestätigen</button>
+
                     <table class='table table-striped'>
                         <tr>
                             <th>ID</th>
@@ -105,17 +125,59 @@
                             echo "<td>" . $row["Email"] . "</td>";
                             echo "<td>";
                             echo "<select name='ZugriffsrechteID[" . $row["BenutzerID"] . "]'>";
-                                echo "<option value='1' " . ($row["ZugriffsrechteID"] == 1 ? "selected" : "") . ">Nutzer</option>";
-                                echo "<option value='2' " . ($row["ZugriffsrechteID"] == 2 ? "selected" : "") . ">Tutor</option>";
-                                echo "<option value='3' " . ($row["ZugriffsrechteID"] == 3 ? "selected" : "") . ">Admin</option>";
+                            echo "<option value='1' " . ($row["ZugriffsrechteID"] == 1 ? "selected" : "") . ">Nutzer</option>";
+                            echo "<option value='2' " . ($row["ZugriffsrechteID"] == 2 ? "selected" : "") . ">Tutor</option>";
+                            echo "<option value='3' " . ($row["ZugriffsrechteID"] == 3 ? "selected" : "") . ">Admin</option>";
                             echo "</select>";
                             echo "</td>";
                             echo "</tr>";
                         }
                         ?>
                     </table>
-                    <button class="button-short" type="submit">Änderungen bestätigen</button>
                 </form>
+                <div class=container>
+                    <div class="row">
+                        <div class="col">
+                            <form action="userManagement.php" method="get">
+                                <input type="hidden" name="Page" value=<?php echo $nextPage ?>>
+                                <!-- Hier die Seitenzahl entsprechend aktualisieren -->
+                                <button class="button-short" type="submit" <?php if ($page == $lastPage) {
+                                    echo "disabled";
+                                } ?>>Weiter</button>
+                            </form>
+                            </div>
+                            <div class="col">
+
+                            <form id="pages" action="userManagement.php" method="get">
+                                <select name="Page" id="changePage">
+                                    <?php for ($i = 1; $i <= $lastPage; $i++) {
+                                        echo "<option value='" . $i . "'";
+                                        if ($i == $page) {
+                                            echo " selected"; // Das selected-Attribut wird hinzugefügt, wenn $i gleich $page ist
+                                        }
+                                        echo ">" . $i . "</option>";
+                                    } ?>f
+
+                                </select>
+                                <script>
+                                    document.getElementById("changePage").addEventListener("change", function () {
+                                        document.getElementById("pages").submit();
+                                    });
+                                </script>
+                            </form>
+                            </div>
+
+                        <div class="col">
+                            <form action="userManagement.php" method="get">
+                                <input type="hidden" name="Page" value=<?php echo $previousPage ?>>
+                                <!-- Hier die Seitenzahl entsprechend aktualisieren -->
+                                <button class="button-short" type="submit" <?php if ($page == 1) {
+                                    echo "disabled";
+                                } ?>>Zurück</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
 
                 <form action="userManagement.php" method="GET">
                     <label for="Vorname">Vorname:</label><br>
@@ -127,7 +189,7 @@
                     <label for="Email">E-mail:</label><br>
                     <input type="text" id="Email" name="Email"><br><br>
 
-                    <input class="button-short" accept=""type="submit" value="Absenden">
+                    <input class="button-short" accept="" type="submit" value="Absenden">
                 </form>
             </div>
             <div class="col-1"> </div>
