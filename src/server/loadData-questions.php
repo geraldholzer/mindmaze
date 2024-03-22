231f24cc06db6830501710c43010450be08c671e
Andreas <andreas.gschliffner@iubh-fernstudium.de>
2024-03-22 22:44:03 +0100

    Version Frage freigeben (Tabelle, etc)

--- src/server/loadData-questions.php
+++ src/server/loadData-questions.php
@@ -1,6 +1,7 @@
 <?php
-  // Überprüfen, ob das Formular gesendet wurde
+  // Überprüfen, ob und welche Request-Methode versendet wurde
   if (isset($_SERVER['REQUEST_METHOD']) && ($_SERVER["REQUEST_METHOD"] == "POST")) {
+    //Funktion zum ermitteln von Kurse lt. Studiengang und Benutzer und erstellen von Comboboxeinträgen
     if (isset($_POST['action']) && ($_POST['action']) == 'getKurseByStudiengang') {
       include "../html-php-view/dbconnect.php";
       try {
@@ -8,8 +9,7 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   
         session_start();
-        //Normaler User soll alle Kurse des Studiengangs sehen
-
+        //Student: soll alle Kurse des Studiengangs sehen
         if ($_SESSION['ZugriffsrechteID'] == 1){ 
           $studiengang = $_POST['studiengangID'];
           // Durchführen der SQL Abfrage
@@ -22,8 +22,9 @@
           while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<option value='" . $row["KursID"] . "'>" . $row["Beschreibung"] . "</option>";
           }
-        //Tutor soll alle Kurse sehen vom Studiengang bei dem er als Benutzer eingetragen ist
-        }else if ($_SESSION['ZugriffsrechteID'] == 2){ 
+        //Tutor: soll alle Kurse sehen vom Studiengang bei dem er als Benutzer eingetragen ist
+        }
+        else if ($_SESSION['ZugriffsrechteID'] == 2){ 
           $studiengang = $_POST['studiengangID'];
           $userID = $_SESSION['BenutzerID'];
           // Durchführen der SQL Abfrage
@@ -37,9 +38,10 @@
     
           while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<option value='" . $row["KursID"] . "'>" . $row["Beschreibung"] . "</option>";
-          }         
+          }  
+        //Admin soll alles sehen       
         }
-        if ($_SESSION['ZugriffsrechteID'] == 3){ 
+        else if ($_SESSION['ZugriffsrechteID'] == 3){ 
           $studiengang = $_POST['studiengangID'];
           $userID = $_SESSION['BenutzerID'];
           // Durchführen der SQL Abfrage
@@ -52,6 +54,51 @@
           while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             echo "<option value='" . $row["KursID"] . "'>" . $row["Beschreibung"] . "</option>";
           }         
+        }
+      } catch (PDOException $e) {
+        echo "Fehler: " . $e->getMessage();
+      }
+    }
+
+    //Funktion zum Ermittln der Fragen je Kust und erstellen einer Tabelle
+    if (isset($_POST['action']) && ($_POST['action']) == 'getFragenByKurs') {
+      include "../html-php-view/dbconnect.php";
+      try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+        $kursID = $_POST['kursID'];
+        // Durchführen der SQL Abfrage
+        $stmtquestion = $conn->prepare("SELECT fragen.*, fragentyp.Beschreibung AS FragenTypBesch FROM fragen LEFT JOIN fragentyp ON fragen.FragentypID = fragentyp.FragentypID
+                                                                                                  WHERE fragen.KursID = :kurs");
+        $stmtquestion->bindParam(':kurs', $kursID);
+        $stmtquestion->execute();
+  
+        while ($question = $stmtquestion->fetch(PDO::FETCH_ASSOC)) {
+          $stmtanswer = $conn->prepare("SELECT antworten.* FROM antworten WHERE antworten.FragenID = :question");
+          $stmtanswer->bindParam(':question', $question["FragenID"]);
+          $stmtanswer->execute();
+
+          echo "<tr>";
+          echo "  <td>" . $question["FragenID"]  . "</td>";
+          echo "  <td>" . $question["FragenTypBesch"] . "</td>";
+          echo "  <td>" . $question["FrageText"] . "</td>";
+          echo "  <td>" . $question["InfoText"]  . "</td>";
+          if ($question["FragentypID"] == 1){
+            echo "  <td></td>";  
+          }
+          while ($answer = $stmtanswer->fetch(PDO::FETCH_ASSOC)) {
+            echo "  <td>" . $answer["Text"]  . "</td>";
+          }
+          if ($question["FragentypID"] == 2){
+            echo "  <td></td>";  
+            echo "  <td></td>";  
+            echo "  <td></td>";  
+            echo "  <td></td>";  
+          } 
+          echo "  <td><button onclick='discardAnswer(". $question["FragenID"] . "); event.preventDefault()'>Ablehnen</td>";
+          echo "  <td><button onclick='acceptAnswer(" . $question["FragenID"] . "); event.preventDefault()'>Freigeben</td>";
+          echo "</tr>";  
         }
       } catch (PDOException $e) {
         echo "Fehler: " . $e->getMessage();
